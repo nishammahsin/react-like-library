dc22534782f2be9421c2b46fb4ca9c1c897da4b6
Nisham <nisham.mahsin@gmail.com>
2023-10-04 17:16:43 +0000

    Add chapter 3 ; use babel to transform jsx to javascript

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,7 +1,12 @@
+
 import { createRoot } from '../react-dom/index.js';
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-const app = '<div>Hello world</div>'
+const app = () => {   
+    return <div>
+        <h1>hello world</h1>
+    </div>
+}
 
 root.render(
     app
